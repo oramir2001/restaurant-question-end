eee03ca02a2698e846b8e23c63840381192a1c65
Your Name <you@example.com>
2023-04-19 13:31:36 +0300

    after build all the models without connect

--- food_ordering_guest/models.py
+++ food_ordering_guest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -6,4 +7,37 @@ class FoodOrderingGuest(models.Model):
   user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
   phone = models.CharField(max_length=10)
   address = models.CharField(max_length=20)
+  age = models.CharField(max_length=20)
   id = models.CharField(max_length=9)
+
+class Category(models.Model):
+  name = models.CharField(max_length=30)
+  image = models.CharField(max_length=500)
+
+class Dish(models.Model):
+  name = models.CharField(max_length=30)
+  price = models.IntegerField(validators=[MaxValueValidator(999999999999999)])
+  descreption = models.CharField(max_length=500)
+  image = models.CharField(max_length=500)
+  is_gluten_free = models.BooleanField(auto_created=False)
+  is_vageterian = models.BooleanField(auto_created=False)
+  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Items(models.Model):
+  dish_id = models.ForeignKey(Dish, on_delete=models.CASCADE)
+  cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+  amount = models.IntegerField(validators=[MaxValueValidator(999999999999999)])
+
+class Delivery(models.Model):
+  is_delivered = models.BooleanField(auto_created=False)
+  address = models.CharField(max_length=500)
+  comment = models.CharField(max_length=1000)
+  created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
