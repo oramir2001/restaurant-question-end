3e54d4490303f377b581ab6c808d14ff3c0640d6
Your Name <you@example.com>
2023-04-19 21:26:29 +0300

    add to sqlite

---
+++ generate_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO food_ordering_guest_category (name, image) VALUES ('starters', '');
+INSERT INTO food_ordering_guest_category (name, image) VALUES ('mains', '');
+INSERT INTO food_ordering_guest_category (name, image) VALUES ('drinks', '');
+INSERT INTO food_ordering_guest_category (name, image) VALUES ('desserts', '');
+
+
+INSERT INTO food_ordering_guest_dish VALUES (name = 'סטייק אנטריקוט', price = '130', image_name = 'Stake.jpeg');
+INSERT INTO food_ordering_guest_dish VALUES (name = 'עוף בגריל', price = '78', image_name = 'Chicken.jpeg');
+INSERT INTO food_ordering_guest_dish VALUES (name = 'שניצל', price = '56', image_name = 'Shnitzel.jpeg');
+INSERT INTO food_ordering_guest_dish VALUES (name = 'פסטה עגבניות', price = '49', image_name = 'TomatoPasta.jpeg');
+
+-- INSERT INTO food_ordering_guest_category (name, image) VALUES ('starters', '');
+-- INSERT INTO food_ordering_guest_category (name, image) VALUES ('mains', '');
+-- INSERT INTO food_ordering_guest_category (name, image) VALUES ('drinks', '');
+-- INSERT INTO food_ordering_guest_category (name, image) VALUES ('desserts', '');
